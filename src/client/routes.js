8d2fd9f45b27d26176113a3459b7cfeec51a7d35
sevenwhite <alum.777.leo@gmail.com>
2017-04-08 22:02:34 +0300

    add no match routes

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {
 	Route,
-	Link
+	Link,
+	Switch
 } from 'react-router-dom';
 
 const Home = () => <h1>Home</h1>;
 const About = () => <h1>About</h1>;
 const Topics = () => <h1>Topics</h1>;
+const NoMatch = ({location}) => <h1>No match <code>{location.pathname}</code></h1>;
 
 const Root = () => (
 	<div>
@@ -14,13 +16,16 @@ const Root = () => (
 			<li><Link to="/">Home</Link></li>
 			<li><Link to="/about">About</Link></li>
 			<li><Link to="/topics">Topics</Link></li>
+			<li><Link to="/no-match">No match route</Link></li>
 		</ul>
 
 		<hr/>
-
-		<Route exact path="/" component={Home}/>
-		<Route path="/about" component={About}/>
-		<Route path="/topics" component={Topics}/>
+		<Switch>
+			<Route exact path="/" component={Home}/>
+			<Route path="/about" component={About}/>
+			<Route path="/topics" component={Topics}/>
+			<Route component={NoMatch} />
+		</Switch>
 	</div>
 );
 
